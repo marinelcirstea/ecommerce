e9f77546423e849db4ad170755f1a12926f5c1b7
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-11-30 10:17:46 +0000

    laid out empty product service

---
+++ src/services/product-service/index.ts
@@ -0,0 +1,14 @@
+async function createProduct() {}
+
+async function getProduct() {}
+
+async function updateProduct() {}
+
+async function deleteProduct() {}
+
+export default Object.freeze({
+  createProduct,
+  getProduct,
+  updateProduct,
+  deleteProduct,
+});
