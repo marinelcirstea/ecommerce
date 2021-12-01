2d5c48d90eb0935a25ef65f5cee4e43f444437ab
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-01 15:30:43 +0000

    +createProduct

--- src/services/product-service/index.ts
+++ src/services/product-service/index.ts
@@ -1,4 +1,21 @@
-async function createProduct() {}
+import { IProductModel } from "@interfaces/";
+import { productCollection } from "@services/db-collection-factory";
+import { makeProduct } from "./factory";
+
+//
+// TODO: Add relevant types
+//
+
+async function createProduct(reqBody: IProductModel) {
+  // this is more like validation, not an actual factory..
+  // it could actually just go through validation without returning anything
+  // Unless we decide to create the slugs server-side or do some future changes
+  const validProduct = await makeProduct(reqBody);
+
+  const newProduct = await productCollection.createOne(validProduct);
+
+  return newProduct;
+}
 
 async function getProduct() {}
 
