964c9d0c2486c64fc52093e7fd5537b64465e33d
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-01 14:53:47 +0000

    +product config to global config export

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,10 +1,15 @@
 import criticalConfig from "./critical-config";
 import userConfig from "./user-config";
+import productConfig from "./product-configs";
 
 interface IGlobalConfig {
   [key: string]: any;
 }
 
-const globalConfig: IGlobalConfig = Object.freeze({ ...criticalConfig, ...userConfig });
+const globalConfig: IGlobalConfig = Object.freeze({
+  ...criticalConfig,
+  ...userConfig,
+  ...productConfig,
+});
 
 export default globalConfig;
