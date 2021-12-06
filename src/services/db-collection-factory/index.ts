7d0862df2b99f4bbaf614e130974665484466c3b
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-06 11:20:47 +0000

    +category collection

--- src/services/db-collection-factory/index.ts
+++ src/services/db-collection-factory/index.ts
@@ -1,8 +1,10 @@
+import CategoryModel from "@models/category-model";
 import ProductModel from "@models/product-model";
 import UserModel from "@models/user-model";
 import useCollection from "./use-collection";
 
 const userCollection = useCollection(UserModel);
 const productCollection = useCollection(ProductModel);
+const categoryCollection = useCollection(CategoryModel);
 
-export { userCollection, productCollection };
+export { userCollection, productCollection, categoryCollection };
