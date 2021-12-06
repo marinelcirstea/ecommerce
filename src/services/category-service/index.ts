dc3d1ae1b70525412da560225bae25fec0d89211
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-06 11:38:59 +0000

    +getCategory

--- src/services/category-service/index.ts
+++ src/services/category-service/index.ts
@@ -1,4 +1,6 @@
 import { IFilterOptions, ICategoryModel } from "@interfaces/";
+import CustomError from "@libs/custom-error";
+import obj from "@libs/object-utils";
 import { categoryCollection } from "@services/db-collection-factory";
 import { makeCategory } from "./factory";
 
@@ -17,7 +19,35 @@ async function createCategory(reqBody: ICategoryModel) {
   return newCategory;
 }
 
-async function getCategory(filter: any, options?: IFilterOptions) {}
+async function getCategory(filter: any, options?: IFilterOptions) {
+  if (!filter || !Object.keys(filter)[0]) {
+    throw new Error("Empty object passed as collection filter.");
+  }
+  const category = await categoryCollection.findOne(filter);
+
+  if (!category) {
+    throw new CustomError("Category not found.", 404);
+  }
+
+  if (!options) return category;
+
+  const { exclude, pick } = options;
+
+  // you can't include and exclude fields at the same time.
+  if (exclude && pick) {
+    throw new Error("You can either use `options.exclude` or `options.pick`. NOT BOTH!");
+  }
+
+  // delete everything passed in 'options.exclude' array
+  if (exclude && exclude[0]) {
+    return obj(category).exclude(exclude);
+  }
+
+  // delete everything, except what's in "options.pick" array
+  if (pick && pick[0]) {
+    return obj(category).pick(pick);
+  }
+}
 
 async function updateCategory(filter: any, data: any) {}
 
