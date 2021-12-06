41daeea8a89129105a446afd1cd4cc7870a50540
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-06 11:29:15 +0000

    +initial category configs

---
+++ src/configs/category-configs.ts
@@ -0,0 +1,10 @@
+// let's keep it the same as product config
+const categoryConfig = {
+  CATEGORY_TITLE_MIN_LENGTH: 1,
+  CATEGORY_TITLE_MAX_LENGTH: 150,
+  CATEGORY_DESCRIPTION_MIN_LENGTH: 1,
+  CATEGORY_DESCRIPTION_MAX_LENGTH: 5000,
+  CATEGORY_DEFAULT_DESCRIPTION: "This category doesn't have any description.",
+};
+
+export default categoryConfig;
