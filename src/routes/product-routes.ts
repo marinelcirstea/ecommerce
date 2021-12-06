377321be16c825d99472af458ec3bb9dd3f8fa7b
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-06 07:55:33 +0000

    + initial product routes

--- src/routes/product-routes.ts
+++ src/routes/product-routes.ts
@@ -1,11 +1,39 @@
 import { Router } from "express";
 import catchException from "@middlewares/catch-exception";
 import productController from "@controllers/product-controller";
+import { authMiddleware } from "@middlewares/auth-middleware";
 
 export const productRoutes = Router();
 
 /**
  * Create a new product
  * @access  public (only for testing)
+ * TODO     make private
  */
 productRoutes.post("/products", catchException(productController.createProduct));
+
+/**
+ * Get product
+ * @access  public
+ */
+productRoutes.get("/products/slug", catchException(productController.getProduct));
+
+/**
+ * Update product
+ * @access  private
+ */
+productRoutes.put(
+  "/products/productId",
+  authMiddleware,
+  catchException(productController.updateProduct)
+);
+
+/**
+ * Delete product
+ * @access  private
+ */
+productRoutes.delete(
+  "/products/productId",
+  authMiddleware,
+  catchException(productController.deleteProduct)
+);
