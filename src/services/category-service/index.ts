320115361178ca5031f6b106968287252ccf1cb0
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-06 11:37:40 +0000

    +createCategory

--- src/services/category-service/index.ts
+++ src/services/category-service/index.ts
@@ -1,10 +1,21 @@
 import { IFilterOptions, ICategoryModel } from "@interfaces/";
+import { categoryCollection } from "@services/db-collection-factory";
+import { makeCategory } from "./factory";
 
 //
 // TODO: Add relevant types
 //
 
-async function createCategory(reqBody: ICategoryModel) {}
+async function createCategory(reqBody: ICategoryModel) {
+  // this is more like validation, not an actual factory..
+  // it could actually just go through validation without returning anything
+  // Unless we decide to create the slugs server-side or do some future changes
+  const validCategory = makeCategory(reqBody);
+
+  const newCategory = await categoryCollection.createOne(validCategory);
+
+  return newCategory;
+}
 
 async function getCategory(filter: any, options?: IFilterOptions) {}
 
