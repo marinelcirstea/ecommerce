a12086fcbee0ec2e4f2b86cb569968b51f6f9f59
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-10 14:35:36 +0000

    +shipping controller

---
+++ src/controllers/shipping-controller.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from "express";
+import shippingService from "../services/shipping-service";
+
+async function createShippingOption(req: Request, res: Response) {
+  await shippingService.createShippingOption(req.body);
+
+  return res.status(200).json({ success: true, message: "Shipping option created successfully." });
+}
+
+async function getShippingOption(req: Request, res: Response) {
+  const shipping = await shippingService.getShippingOption(req.params.id);
+
+  return res
+    .status(200)
+    .json({ success: true, message: "Shipping option fetched successfully.", data: shipping });
+}
+
+async function getAllShippingOptions(_req: Request, res: Response) {
+  const shipping = await shippingService.getAllShippingOptions();
+
+  return res
+    .status(200)
+    .json({ success: true, message: "Shipping options fetched successfully.", data: shipping });
+}
+
+async function updateShippingOption(req: Request, res: Response) {
+  await shippingService.updateShippingOption(req.params.id, req.body);
+
+  return res.status(200).json({ success: true, message: "Shipping option updated successfully." });
+}
+
+async function deleteShippingOption(req: Request, res: Response) {
+  await shippingService.deleteShippingOption(req.params.id);
+
+  return res.status(200).json({ success: true, message: "Shipping option deleted successfully." });
+}
+
+export default {
+  createShippingOption,
+  getShippingOption,
+  getAllShippingOptions,
+  updateShippingOption,
+  deleteShippingOption,
+};
