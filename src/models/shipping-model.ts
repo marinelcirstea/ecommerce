e39b9827fd90fdb3827f7b6ff673ef0ad3d7860e
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-10 14:35:49 +0000

    + initial shipping model

---
+++ src/models/shipping-model.ts
@@ -0,0 +1,14 @@
+import { IShippingDocument } from "@interfaces";
+import { model, Schema } from "mongoose";
+
+const ShippingSchema = new Schema<IShippingDocument>(
+  {
+    title: { type: String, required: true },
+    price: { type: Number, default: 0 },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const ShippingModel = model<IShippingDocument>("Shipping", ShippingSchema);
+
+export default ShippingModel;
