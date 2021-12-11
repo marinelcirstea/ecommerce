ae33c18c7c8b8a3de87a3413131c35cbd514e499
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-11 00:15:16 +0000

    +cart-controller

---
+++ src/controllers/cart-controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import cartService from "@services/cart-service";
+
+async function createCart(req: Request, res: Response) {
+  const session = res.locals.token;
+  const cart = await cartService.createCart(req.body, session?.userId || "");
+
+  return res.status(200).json({ success: true, message: "Cart created successfully.", data: cart });
+}
+
+async function getCart(req: Request, res: Response) {
+  const cart = await cartService.getCart(req.params.id);
+
+  return res.status(200).json({ success: true, message: "Cart fetched successfully.", data: cart });
+}
+
+async function updateCart(req: Request, res: Response) {
+  await cartService.updateCart(req.params.id, req.body);
+
+  return res.status(200).json({ success: true, message: "Cart updated successfully." });
+}
+
+export default Object.freeze({
+  createCart,
+  getCart,
+  updateCart,
+});
