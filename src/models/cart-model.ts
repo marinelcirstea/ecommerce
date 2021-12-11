d804ec59722ec45b5a378b679a68cf79355534cd
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-11 00:15:58 +0000

    +cart-model

---
+++ src/models/cart-model.ts
@@ -0,0 +1,21 @@
+import { ICartDocument } from "@interfaces";
+import { model, Schema } from "mongoose";
+
+const CartSchema = new Schema<ICartDocument>(
+  {
+    items: [
+      {
+        // populated with title, _id and price
+        item: { type: Schema.Types.ObjectId, ref: "Product" },
+        quantity: { type: Number, default: 1 },
+      },
+    ],
+    userId: { type: String }, // pointless to create a refference to User collection
+    deviceId: { type: String },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const CartModel = model<ICartDocument>("Cart", CartSchema);
+
+export default CartModel;
