1ab8c78e5b8e6d33c182db9974c116a88c6d781e
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-12 23:14:31 +0000

    +stripe keys

--- src/configs/critical-config.ts
+++ src/configs/critical-config.ts
@@ -3,6 +3,8 @@ const criticalConfig = Object.freeze({
   ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET!,
   REFRESH_TOKEN_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET!,
   MONGODB_URI: process.env.MONGODB_URI!,
+  STRIPE_PRIVATE_KEY: process.env.STRIPE_PRIVATE_KEY!,
+  STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY!,
 });
 
 export default criticalConfig;
