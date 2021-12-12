51b6895c380df2d46e009ab1f03af7af9c77e644
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-12 23:22:37 +0000

    +initial order service(not done)

---
+++ src/services/order-service/index.ts
@@ -0,0 +1,42 @@
+import { IOrderModel, KeysOfModel } from "@interfaces/";
+import CustomError from "@libs/custom-error";
+import Order from "@models/order-model";
+
+async function createOrder(orderData: IOrderModel) {
+  //todo: Create validation
+  const newOrder = new Order(orderData);
+
+  await newOrder.save();
+
+  return newOrder;
+}
+
+async function getOrder(id: string) {
+  const order = await Order.findOne({ _id: id }).lean();
+
+  if (!order) {
+    throw new CustomError("Order not found.", 404);
+  }
+
+  return order;
+}
+
+// async function getOrders(range:any){}
+
+async function updateOrder(id: string, data: KeysOfModel<IOrderModel>) {
+  const ack = await Order.updateOne({ _id: id }, data, { upsert: false });
+
+  if (!ack.acknowledged) {
+    // TODO: add critical error logging
+    throw new CustomError(`Failed to update order.`, 400);
+  }
+}
+
+// should we create a deleteOrder function?
+// async function deleteOrder(){}
+
+export default Object.freeze({
+  createOrder,
+  getOrder,
+  updateOrder,
+});
