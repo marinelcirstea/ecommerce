9df79ca5252cb3a9d77fbc02d8fe576d8b7864d0
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-13 18:28:43 +0000

    updated order status

--- src/models/order-model.ts
+++ src/models/order-model.ts
@@ -3,8 +3,8 @@ import { model, Schema } from "mongoose";
 
 const OrderSchema = new Schema<IOrderDocument>(
   {
-    user: { type: Schema.Types.ObjectId, ref: "User" },
-    items: [],
+    user: { type: String },
+    cart: { type: Schema.Types.ObjectId, ref: "Cart" },
     itemsTotal: { type: Number, required: true },
     shipping: { type: Schema.Types.ObjectId, ref: "Shipping" },
     address: {
@@ -16,6 +16,26 @@ const OrderSchema = new Schema<IOrderDocument>(
       postalCode: { type: String },
       email: { type: String },
       phone: { type: String },
+    },
+    status: {
+      type: String,
+      enum: [
+        "pending", // Customer started the checkout process but did not complete it.
+        "awaiting_payment", // Customer has completed the checkout process, but payment has yet to be confirmed.
+        "payment_failed", // Customer has completed the checkout process, but payment has failed.
+        "paid", // Customer has completed the checkout process and payment has been confirmed.
+        "fulfilled", // Order has been picked and packed and is awaiting collection from a shipping provider.
+        "shipped", // Order has been shipped, but receipt has not been confirmed.
+        "partially_shipped", // Only some items in the order have been shipped.
+        "completed", // Order has been shipped/picked up, and receipt is confirmed.
+        "cancelled", // Seller/customer has cancelled an order.
+        "declined", // Seller has marked the order as declined.
+        "refunded", // Seller has refunded the whole order.
+        "partially_refunded", // Seller has partially refunded the order.
+        "disputed", // Customer/Seller has initiated a dispute resolution process for the transaction.
+        "manual_verification_required", // Order on hold while some aspect, such as tax-exempt documentation, is manually confirmed.
+      ],
+      default: "pending",
     },
     total: { type: Number },
   },
