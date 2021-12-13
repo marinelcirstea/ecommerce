aca305602455a78c98647cd1586baa03de9301e9
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-13 18:26:33 +0000

    updated cart controller

--- src/controllers/cart-controller.ts
+++ src/controllers/cart-controller.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from "express";
 import cartService from "@services/cart-service";
 
-async function createCart(req: Request, res: Response) {
-  const session = res.locals.token;
-  const cart = await cartService.createCart(req.body, session?.userId || "");
+async function createOrUpdateCart(req: Request, res: Response) {
+  const cart = await cartService.createOrUpdateCart(req.body);
 
-  return res.status(200).json({ success: true, message: "Cart created successfully.", cart });
+  return res.status(200).json({ success: true, message: "Cart updated", cart });
 }
 
 async function getCart(req: Request, res: Response) {
@@ -14,14 +13,7 @@ async function getCart(req: Request, res: Response) {
   return res.status(200).json({ success: true, message: "Cart fetched successfully.", cart });
 }
 
-async function updateCart(req: Request, res: Response) {
-  await cartService.updateCart(req.params.id, req.body);
-
-  return res.status(200).json({ success: true, message: "Cart updated successfully." });
-}
-
 export default Object.freeze({
-  createCart,
+  createOrUpdateCart,
   getCart,
-  updateCart,
 });
