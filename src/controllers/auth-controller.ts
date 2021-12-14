e991f6ef8ccab4694562215f613439fa4009ab25
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-14 16:27:17 +0000

    changed to @services

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import authService from "../services/auth-service";
+import authService from "@services/auth-service";
 
 const login = async (req: Request, res: Response) => {
   await authService.loginWithEmailAndPassword(req.body, res);
