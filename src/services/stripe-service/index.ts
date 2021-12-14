f47b4d200bca98fde69c7bc67f318f137994e972
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-14 00:41:47 +0000

    updated stripe service

--- src/services/stripe-service/index.ts
+++ src/services/stripe-service/index.ts
@@ -2,17 +2,13 @@ import CustomError from "@libs/custom-error";
 import stripe from "./stripe-api";
 import GC from "@configs";
 
-async function createPaymentIntent(body: { [key: string]: any }) {
+async function createPaymentIntent(amount: number, metadata: { [key: string]: any } = {}) {
   try {
-    // const { currency } = body;
-    console.log(body);
     const paymentIntent = await stripe.paymentIntents.create({
       payment_method_types: ["card"],
-      amount: 120,
+      amount: amount * 100,
       currency: "usd",
-      metadata: {
-        orderId: "myorderid",
-      },
+      metadata,
     });
     console.log(paymentIntent);
     return paymentIntent.client_secret;
