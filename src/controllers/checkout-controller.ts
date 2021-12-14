a132dcfb19af126469314a89172539eb74fcaf6d
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-14 00:42:55 +0000

    +checkout controller

---
+++ src/controllers/checkout-controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import checkoutService from "@services/checkout-service";
+
+// Checkout flow ?
+// client-> post request to create an order
+// create order and set the payment status to "awaiting_payment"
+// send back the clientSecret from stripe API and initiate checkout
+// create webhooks and liten to changes in the payment process
+// when stripe accepts the payment, activate the order and send email
+async function checkout(req: Request, res: Response) {
+  console.log("req.body:", req.body);
+  const session = res.locals.token;
+
+  const order = await checkoutService.checkout(req.body, session?.userId);
+
+  return res
+    .status(200)
+    .json({ success: true, message: "Order created successfully.", data: order });
+}
+
+export default Object.freeze({
+  checkout,
+});
