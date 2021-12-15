98c344d0d5f710a30ef1e1c96bb7dbc13530f660
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-15 22:55:00 +0000

    - comments

--- src/middlewares/assign-token-to-locals.ts
+++ src/middlewares/assign-token-to-locals.ts
@@ -2,12 +2,6 @@ import { NextFunction, Request, Response } from "express";
 import { Cookies } from "@interfaces";
 import { verifyAccessToken } from "@libs/jwt";
 
-// Used for carts controller
-
-// if the token exists, the user is signed in, so we'll
-// assign the userId to a cart
-
-// if not, checkout as guest
 export function assignTokenToLocals(req: Request, res: Response, next: NextFunction) {
   try {
     const token = verifyAccessToken(req.cookies[Cookies.AccessToken]);
