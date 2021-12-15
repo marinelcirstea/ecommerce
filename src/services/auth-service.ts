dc58836ba95884278c4c8ca1e883b90a7ca521b7
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-15 22:55:54 +0000

    + refresh and increaseToken functions

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,6 +1,6 @@
 import { Response } from "express";
 import CustomError from "@libs/custom-error";
-import { buildTokens, clearTokens, setTokens } from "@libs/jwt";
+import { buildTokens, clearTokens, refreshTokens, setTokens, verifyRefreshToken } from "@libs/jwt";
 import { compare } from "@libs/string-utils/hash-and-compare";
 import { isEmail, isString } from "@libs/string-utils";
 import User from "@models/user-model";
@@ -32,7 +32,34 @@ const logoutUser = (res: Response) => {
   clearTokens(res);
 };
 
+const refresh = async (refreshTokenFromReq: string, res: Response) => {
+  try {
+    const current = verifyRefreshToken(refreshTokenFromReq);
+
+    const user = await User.findOne({ _id: current.userId });
+    if (!user) throw "User not found";
+
+    const { accessToken, refreshToken } = refreshTokens(current, user.tokenVersion);
+    setTokens(res, accessToken, refreshToken);
+  } catch (error) {
+    clearTokens(res);
+  }
+};
+
+export async function increaseTokenVersion(userId: string) {
+  const result = await User.findOneAndUpdate(
+    { _id: userId },
+    { $inc: { tokenVersion: 1 } },
+    { upsert: false, new: true }
+  ).lean();
+  if (!result) {
+    throw new Error("Failed to increment token version.");
+  }
+  return result;
+}
+
 export default {
+  refresh,
   loginWithEmailAndPassword,
   logoutUser,
 };
