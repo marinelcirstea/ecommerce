258ac804b13dc91c3886fcac56b06d1716c2d2e8
Marinel Cirstea <marinel.cirstea@gmail.com>
2021-12-15 22:53:33 +0000

    +refresh fn

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,19 +1,27 @@
 import { Request, Response } from "express";
 import authService from "@services/auth-service";
+import { Cookies } from "@interfaces/";
 
 const login = async (req: Request, res: Response) => {
   await authService.loginWithEmailAndPassword(req.body, res);
 
-  return res.status(200).json({ success: true, message: "Login successful." });
+  return res.end();
+};
+
+const refresh = async (req: Request, res: Response) => {
+  await authService.refresh(req.cookies[Cookies.RefreshToken], res);
+
+  return res.end();
 };
 
 const logout = async (_req: Request, res: Response) => {
   authService.logoutUser(res);
 
-  return res.status(200).json({ success: true, message: "Logout successful." });
+  return res.end();
 };
 
 export default {
+  refresh,
   login,
   logout,
 };
